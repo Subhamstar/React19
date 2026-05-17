ed012243774c91c8404ec7f549bfa97c1b6cd475
Subhamstar <mr.subhamdas999@gmail.com>
2026-05-17 14:34:49 +0530

    updated swc

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 import React from "react";
-const heading=React.createElement("h1",{},"Hello form React JS");
+import ReactDOM from "react-dom/client";
 
-const Title = ()=>(
-    <h1 className="head">This is Title component ..</h1>
-)
-const HeadingCompont=()=>(
-    <div id="container">
-        {heading}
-        <Title/>
-        <h1>Namast react functional component </h1>
-    </div>
-)
+const heading = React.createElement(
+  "h1",
+  {},
+  "Hello from React JS"
+);
 
+const Title = () => (
+  <h1 className="head">This is Title component ..</h1>
+);
 
+const HeadingComponent = () => (
+  <div id="container">
+    {heading}
+    {/* <Title /> */}
+    {Title()}
+    <h1>Namaste React functional component</h1>
+  </div>
+);
 
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
-
-
-
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingCompont/>);
+root.render(<HeadingComponent />);
